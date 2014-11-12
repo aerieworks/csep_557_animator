bd0fc8f699549666fb3695b4c1d0d783a93b7179
Annabelle Richard <annabelle@Annabelles-MacBook-Air.local>
2014-11-11 17:27:51 -0800

    Initial Commit

---
+++ Animator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Animator
+//
+//  Created by Annabelle Richard on 11/11/14.
+//  Copyright (c) 2014 Annabelle Richard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
